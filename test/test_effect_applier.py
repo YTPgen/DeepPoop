f627b38abbb99dee9f4a57af059d3a5ab1887c53
Karl Gylleus <gylleus@kth.se>
2020-07-10 23:58:47 +0200

    Mock effect in effect applier test

--- test/test_effect_applier.py
+++ test/test_effect_applier.py
@@ -1,34 +1,34 @@
+from unittest.mock import MagicMock
+
 import pytest
 
+from deep_poop.effects.effect import Effect
 from deep_poop.effect_applier import EffectApplier
 from deep_poop.clips.cut_clip import CutClip
 from test.utils import scene_frames_identical
 
 
 @pytest.fixture(scope="session")
-def effect_applier():
-    return EffectApplier(20, 0)
+def dummy_effect():
+    dummy_effect = Effect(intensity=1, effect_type=None)
+    dummy_effect.apply = MagicMock()
+    return dummy_effect
+
+
+@pytest.fixture(scope="session")
+def effect_applier(dummy_effect):
+    return EffectApplier(max_intensity=20, easy_start=0, effects=[dummy_effect])
 
 
 def test_effect_applier_skip(effect_applier, scene):
     effect_applier.max_intensity = 20
     effect_applier.intensity = 30
-    frames_before = CutClip(scene.clip).frames
-    changed_clip = effect_applier.feed_scene(scene)
-    frames_after = CutClip(scene.clip).frames
-    assert scene_frames_identical(
-        frames_before, frames_after
-    ), "Scene object clip is different after effect applier. Should only return altered copy."
+    effect_applier.feed_scene(scene)
+    effect_applier.effects[0].apply.assert_not_called()
 
 
 def test_effect_applier_applied(effect_applier, scene):
-    frames_before = CutClip(scene.clip).frames
-    changed_clip = effect_applier.feed_scene(scene)
-    frames_after = CutClip(scene.clip).frames
-    assert scene_frames_identical(
-        frames_before, frames_after
-    ), "Scene object clip is different after effect applier. Should only return altered copy."
-    assert not scene_frames_identical(
-        frames_before, CutClip(changed_clip).frames
-    ), "Scene after effect is identical to before effect applier"
+    effect_applier.intensity = 0
+    effect_applier.feed_scene(scene)
+    effect_applier.effects[0].apply.assert_called()
 
